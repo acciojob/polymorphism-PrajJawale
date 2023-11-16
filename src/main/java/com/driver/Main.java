8dca8cc9210a57bd8435bc4895d512b2be3fff55
Praj1203 <prajwaljawale2k27@gmail.com>
2023-11-16 18:46:32 +0530

    polymorphism2

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,9 +17,10 @@ public class Main {
 
     public static void main(String[] args) {
         Product p = new Product();
-        p.product(10,20);
-        p.product(10,20,30);
-        p.product(10.00,20.00);
+        int ans1 = p.product(10,20);
+        int ans2 = p.product(10,20,30);
+        double ans3 = p.product(10.00,20.00);
+        System.out.println(ans1+" "+ans2+" "+ans3);
 
     }
 }
